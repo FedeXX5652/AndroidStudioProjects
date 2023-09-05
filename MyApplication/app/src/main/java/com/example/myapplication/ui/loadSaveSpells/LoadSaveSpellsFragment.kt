90b7c35cf30a4a26af83ed9151bcd89510e737b5
Federico Galante <54709342+FedeXX5652@users.noreply.github.com>
2023-09-04 22:16:01 -0300

    arreglo list persistencia

--- MyApplication/app/src/main/java/com/example/myapplication/ui/loadSaveSpells/LoadSaveSpellsFragment.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/loadSaveSpells/LoadSaveSpellsFragment.kt
@@ -61,7 +61,9 @@ class LoadSaveSpellsFragment : Fragment() {
             val savedSpellsList = binding.savedSpellsListView
             val savedSpells = mutableListOf<String>()
             for (i in 0 until savedSpellsJSONArray.length()) {
-                savedSpells.add(savedSpellsJSONArray.getJSONObject(i).getString("title").toString())
+                savedSpells.add("${savedSpellsJSONArray.getJSONObject(i).getString("title")}:    " +
+                        savedSpellsJSONArray.getJSONObject(i).getString("description")
+                )
             }
             Log.i("savedSpells", savedSpells.toString())
             savedSpellsList.adapter = ArrayAdapter(
@@ -69,35 +71,64 @@ class LoadSaveSpellsFragment : Fragment() {
                 R.layout.simple_spinner_dropdown_item,
                 savedSpells
             )
+
+            savedSpellsList.setOnItemClickListener{ _, _, position, _ ->
+                val selected = savedSpellsJSONArray.getJSONObject(position)
+                saveTitle.setText(selected.getString("title"))
+                saveDescription.setText(selected.getString("description"))
+                val spellsArray = JSONArray(selected.getString("spells"))
+                for (i in 0 until spellsArray.length()) {
+                    val spell = spellsArray.getJSONObject(i)
+                    activity.spells.add(spell)
+                }
+            }
+            
+            savedSpellsList.setOnItemLongClickListener{ _, _, position, _ ->
+                val selected = savedSpellsJSONArray.getJSONObject(position)
+                val newSavedSpellsJSONArray = JSONArray()
+                for (i in 0 until savedSpellsJSONArray.length()) {
+                    if (i != position) {
+                        newSavedSpellsJSONArray.put(savedSpellsJSONArray.getJSONObject(i))
+                    }
+                }
+                val fileWriter = FileWriter(file)
+                fileWriter.write(newSavedSpellsJSONArray.toString())
+                fileWriter.close()
+                loadButton.performClick()
+            }
         }
 
         saveAccept.setOnClickListener {
             val spells = activity.spells
-            val file = File("${activity.filesDir}/saved.json")
+            if (spells.size > 0 || saveTitle.text.toString() != ""){
+                val file = File("${activity.filesDir}/saved.json")
 
-            // load the saved spells
-            val savedSpellsString = file.bufferedReader().use { it.readText() }
-            val savedSpellsJSONArray = JSONArray(savedSpellsString)
+                Log.i("file", file.absolutePath.toString())
 
-            Log.i("savedSpellsJSONArray", savedSpellsJSONArray.toString())
+                // load the saved spells
+                val savedSpellsString = file.bufferedReader().use { it.readText() }
+                val savedSpellsJSONArray = JSONArray(savedSpellsString)
 
-            // add the new spells
-            val entry = JSONObject()
-            entry.put("title", saveTitle.text.toString())
-            entry.put("description", saveDescription.text.toString())
-            entry.put("spells", spells)
-            savedSpellsJSONArray.put(entry)
-            savedSpellsJSONArray.put(entry)
+                Log.i("savedSpellsJSONArray", savedSpellsJSONArray.toString())
 
-            // open the file
-            val fileWriter = FileWriter(file)
+                // add the new spells
+                val entry = JSONObject()
+                entry.put("title", saveTitle.text.toString())
+                entry.put("description", saveDescription.text.toString())
+                entry.put("spells", spells)
+                savedSpellsJSONArray.put(entry)
+                savedSpellsJSONArray.put(entry)
 
-            // write the JSONArray to the file
-            fileWriter.write(savedSpellsJSONArray.toString())
+                // open the file
+                val fileWriter = FileWriter(file)
 
-            // close the file
-            fileWriter.close()
 
+                // write the JSONArray to the file
+                fileWriter.write(savedSpellsJSONArray.toString())
+
+                // close the file
+                fileWriter.close()
+            }
         }
 
         return root
